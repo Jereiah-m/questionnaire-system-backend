9c964795341f262e879cc20840d5c6536777aa37
Jeremiah <94828537+Jereiah-m@users.noreply.github.com>
2023-03-14 22:35:35 +0800

    Delete define directory

--- define/define.go
+++
@@ -1,8 +0,0 @@
-package define
-
-var MailPassword = "rcukdqfsfdcrbfgg" //邮箱密码
-
-var RegisterPrefix = "TOKEN_"
-var ExpireTime = 300 //过期时间
-
-var CaptchaSecret = "6LfEVIoiAAAAAD1dZ-B79CSfSmp-U12yBy5pgK-f" //验证码秘钥
